af8d98aa2fd1c58f7cd38be71b879052ee88abc0
Gabe Chavez <gabefc@gmail.com>
2017-02-22 14:37:59 -0700

    added request extensions for getting access token value from AccessToken class instead of Account class.

---
+++ src/OAuth2.Mobile/OAuth2RequestExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Auth;
+
+namespace StudioDonder.OAuth2.Mobile
+{
+    public static class OAuth2RequestExtensions
+    {
+        public static Uri GetAuthenticatedUrl(AccessToken token, Uri unauthenticatedUrl, string accessTokenParameterName = "access_token")
+        {
+            if (token == null) throw new ArgumentNullException(nameof(token));
+
+            if (unauthenticatedUrl == null) throw new ArgumentNullException(nameof(unauthenticatedUrl));
+
+            var absUri = unauthenticatedUrl.AbsoluteUri;
+            var url = $"{absUri}{(absUri.Contains("?") ? "&" : "?")}{accessTokenParameterName}={token.Token}";
+            return new Uri(url);
+        }
+
+        public static string GetAuthorizationHeader(this OAuth2Request request, AccessToken token)
+        {
+            if (token == null) throw new ArgumentNullException(nameof(token));
+
+            return $"Bearer {token.Token}";
+        }
+    }
+}
